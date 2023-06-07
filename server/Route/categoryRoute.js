363a16a2719bcfd88dfcdb9957fab51caec66a6d
praveenkrao94 <praveenkr0812@gmail.com>
2023-06-07 08:08:00 +0530

    middleware integrated to Route

--- server/Route/categoryRoute.js
+++ server/Route/categoryRoute.js
@@ -2,13 +2,16 @@ const catRoute = require('express').Router()
 
 const categoryCtrl = require('../controller/categoryController')
 
+const adminMiddleware = require('../middleware/adminMiddleware')
+
+const authmiddleware = require('../middleware/authMiddleware')
 
 
-catRoute.get('/all', categoryCtrl.getall)
-catRoute.get('/single/:id', categoryCtrl.getSingle)
-catRoute.post('/create', categoryCtrl.create)
-catRoute.patch('/update/:id', categoryCtrl.update)
-catRoute.delete('/delete/:id', categoryCtrl.delete)
+catRoute.get('/all', authmiddleware, adminMiddleware, categoryCtrl.getall)
+catRoute.get('/single/:id', authmiddleware, adminMiddleware, categoryCtrl.getSingle)
+catRoute.post('/create', authmiddleware, adminMiddleware, categoryCtrl.create)
+catRoute.patch('/update/:id', authmiddleware, adminMiddleware, categoryCtrl.update)
+catRoute.delete('/delete/:id', authmiddleware, adminMiddleware, categoryCtrl.delete)
 
 
 
